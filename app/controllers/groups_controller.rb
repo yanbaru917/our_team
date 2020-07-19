37cdecb203c9b4036a71d8924e9a57ac29140308
yanbaru917 <ttaakkeemsss@gmail.com>
2020-07-19 23:53:42 +0900

    view-edit
    side-messageの編集

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,7 @@
 class GroupsController < ApplicationController
-
   def index
-
   end
-  
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -25,7 +23,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを編集しました'
+      redirect_to root_path, notice: 'グループを更新しました'
     else
       render :edit
     end
@@ -35,4 +33,5 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:name, user_ids: [])
   end
+
 end
